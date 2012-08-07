f2a993f5e07090b1a3b0d612a7163d85967205c6
TJ Holowaychuk <tj@vision-media.ca>
2012-08-06 21:42:58 -0700

    add test for `component`

---
+++ test/component.js
@@ -0,0 +1,27 @@
+
+/**
+ * Module dependencies.
+ */
+
+var exec = require('child_process').exec
+  , fs = require('fs')
+  , path = require('path');
+
+describe('component', function(){
+  describe('empty', function(){
+    it('should output help', function(done){
+      exec('bin/component', function(err, stdout){
+        if (err) return done(err);
+        stdout.should.include('component <command> [options]');
+        stdout.should.include('--help');
+        stdout.should.include('Commands:');
+        stdout.should.include('install');
+        stdout.should.include('create');
+        stdout.should.include('search');
+        stdout.should.include('info');
+        stdout.should.include('ls');
+        done();
+      })
+    })
+  })
+})
