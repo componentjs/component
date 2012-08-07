4b4b6e631e1986ba0f8f467ff8cce8a1ac4de9b1
TJ Holowaychuk <tj@vision-media.ca>
2012-08-06 20:45:20 -0700

    add some test stubs

---
+++ test/install.js
@@ -0,0 +1,24 @@
+
+/**
+ * Module dependencies.
+ */
+
+var exec = require('child_process').exec;
+
+describe('component-install', function(){
+  describe('[name]', function(){
+    it('should install the given component', function(done){
+      
+    })
+  })
+
+  describe('[name...]', function(){
+    it('should install the given components', function(done){
+      
+    })
+  })
+
+  it('should default to installing from ./component.json', function(done){
+    
+  })
+})
