2a5660602f54d3d07bc9dbc12df353609721692c
TJ Holowaychuk <tj@vision-media.ca>
2012-08-06 20:48:00 -0700

    add first install test

--- test/install.js
+++ test/install.js
@@ -3,12 +3,29 @@
  * Module dependencies.
  */
 
-var exec = require('child_process').exec;
+var exec = require('child_process').exec
+  , path = require('path');
 
 describe('component-install', function(){
+  beforeEach(function(done){
+    exec('rm -fr components', done);
+  })
+
   describe('[name]', function(){
     it('should install the given component', function(done){
-      
+      exec('bin/component-install component/emitter', function(err, stdout){
+        if (err) return done(err);
+        stdout.should.include('install');
+        stdout.should.include('fetch');
+        stdout.should.include('complete');
+        var json = require(path.resolve('components/component-emitter/component.json'));
+        json.name.should.equal('emitter');
+        done();
+      })
+    })
+
+    it('should install dependencies', function(done){
+
     })
   })
 
