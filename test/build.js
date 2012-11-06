5f601fda6023ecec0884b4695eaf1336e3675929
TJ Holowaychuk <tj@vision-media.ca>
2012-11-06 12:59:07 -0800

    replace COMPONENT_PATH with .paths prop. Closes #137

--- test/build.js
+++ test/build.js
@@ -10,34 +10,22 @@ var exec = require('child_process').exec
   , vm = require('vm')
 
 describe('component build', function(){
-  describe('when COMPONENT_PATH is set', function(){
-    it('should build', function(done){
-      exec('cd test/fixtures/path && COMPONENT_PATH=lib ' + bin + '-build -v', function(err, stdout){
-        if (err) return done(err);
-        stdout.should.include('build/build.js');
-        stdout.should.include('duration');
-        stdout.should.include('css');
-        stdout.should.include('js');
+  it('should build', function(done){
+    exec('cd test/fixtures/path && ' + bin + '-build -v', function(err, stdout){
+      if (err) return done(err);
+      stdout.should.include('build/build.js');
+      stdout.should.include('duration');
+      stdout.should.include('css');
+      stdout.should.include('js');
 
-        var js = fs.readFileSync('test/fixtures/path/build/build.js', 'utf8');
-        var ret = vm.runInNewContext(js + '; require("foo")');
-        ret.should.equal('baz');
+      var js = fs.readFileSync('test/fixtures/path/build/build.js', 'utf8');
+      var ret = vm.runInNewContext(js + '; require("foo")');
+      ret.should.equal('baz');
 
-        var ret = vm.runInNewContext(js + '; require("baz")');
-        ret.should.equal('baz');
+      var ret = vm.runInNewContext(js + '; require("baz")');
+      ret.should.equal('baz');
 
-        done();
-      })
-    })
-  })
-
-  describe('when COMPONENT_PATH is omitted', function(){
-    it('should fail', function(done){
-      exec('cd test/fixtures/path && ' + bin + '-build -v', function(err, stdout, stderr){
-        stderr.should.include('error');
-        stderr.should.include('failed to lookup "path"\'s dependency "bar"');
-        done();
-      })
+      done();
     })
   })
 })
