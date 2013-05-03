e0af0d34e0fc1cddec558151e6f0d662df31fae4
TJ Holowaychuk <tj@vision-media.ca>
2013-05-03 13:07:48 -0700

    rename a test

--- test/component.js
+++ test/component.js
@@ -8,21 +8,19 @@ var exec = require('child_process').exec
   , path = require('path');
 
 describe('component', function(){
-  describe('empty', function(){
-    it('should output help', function(done){
-      exec('bin/component', function(err, stdout){
-        if (err) return done(err);
-        stdout.should.include('component <command> [options]');
-        stdout.should.include('--help');
-        stdout.should.include('Commands:');
-        stdout.should.include('install');
-        stdout.should.include('create');
-        stdout.should.include('search');
-        stdout.should.include('info');
-        stdout.should.include('convert');
-        stdout.should.include('ls');
-        done();
-      })
+  it('should output help', function(done){
+    exec('bin/component', function(err, stdout){
+      if (err) return done(err);
+      stdout.should.include('component <command> [options]');
+      stdout.should.include('--help');
+      stdout.should.include('Commands:');
+      stdout.should.include('install');
+      stdout.should.include('create');
+      stdout.should.include('search');
+      stdout.should.include('info');
+      stdout.should.include('convert');
+      stdout.should.include('ls');
+      done();
     })
   })
 })
