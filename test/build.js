e58d63f791692b0eea8b6070e2689c8a5ac04a95
Jonathan Ong <jonathanrichardong@gmail.com>
2014-04-01 17:49:05 -0700

    component-build

--- test/build.js
+++ test/build.js
@@ -3,28 +3,23 @@
  * Module dependencies.
  */
 
-var exec = require('child_process').exec
-  , bin = __dirname + '/../bin/component'
-  , path = require('path')
-  , fs = require('fs')
-  , vm = require('vm')
+var exec = require('child_process').exec;
+var exists = require('fs').existsSync;
+var stat = require('fs').statSync;
+var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
+var vm = require('vm');
+
+var bin = __dirname + '/../bin/component';
 
 describe('component build', function(){
   it('should build', function(done){
     exec('cd test/fixtures/path && ' + bin + '-build', function(err, stdout){
       if (err) return done(err);
-      console.log(stdout)
-      stdout.should.include('build/build.js');
-      stdout.should.include('duration');
-      stdout.should.include('css');
-      stdout.should.include('js');
 
       var js = fs.readFileSync('test/fixtures/path/build/build.js', 'utf8');
-      var ret = vm.runInNewContext(js + '; require("foo")');
-      ret.should.equal('baz');
-
-      var ret = vm.runInNewContext(js + '; require("baz")');
-      ret.should.equal('baz');
+      var ret = vm.runInNewContext(js);
 
       done();
     })
@@ -33,8 +28,9 @@ describe('component build', function(){
   it('should exclude the js file if no scripts, and the css file if no styles', function(done){
     exec('cd test/fixtures/no-js-css && ' + bin + '-build', function(err, stdout){
       if (err) return done(err);
-      stdout.should.not.include('js :');
-      stdout.should.not.include('css :');
+
+      assert(!exists('test/fixtures/no-js-css/build/build.js'))
+      assert(!exists('test/fixtures/no-js-css/build/build.css'))
       done();
     });
   });
